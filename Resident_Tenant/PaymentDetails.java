f7277abaa92c3bf4672de62514f12bcb5f198524
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2023-03-20 13:52:46 +0800

    Create PaymentDetails.java

---
+++ Resident_Tenant/PaymentDetails.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Resident_Tenant;
+
+/**
+ *
+ * @author 60192
+ */
+public interface PaymentDetails {
+
+    default String getName() {
+        return null;
+    }
+
+    default String getCardNumber() {
+        return null;
+    }
+
+    default String getExpiryDate() {
+        return null;
+    }
+
+}
+
